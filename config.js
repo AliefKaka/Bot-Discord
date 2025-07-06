6c296a86ccca3872ba4b14d3683072455fa12926
AliefKaka <aliefkaka123@gmail.com>
2025-07-06 17:20:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // Bot Identity
-  botName: "BetaBotz", // Nama bot kamu
+  botName: "ABSOLUTE", // Nama bot kamu
   ownerID: "Alief Kaka", // Ganti dengan Nama Kamu
 
   // Discord Settings
   token:
     "MTM5MTM1NjAzMDczODIzOTYwOQ.GQU3Gl.JHZQKmlAy5M6Csd4xYVEufJ0lhcnGrmzGVP5ag", // Ganti dengan token bot kamu
   prefix: ".", // Prefix utama untuk bot ini
-  guildID: "wajib di isi", // ID server tempat bot ini berada
+  guildID: "1336379125413318657", // ID server tempat bot ini berada
 
   // API Keys
   apikey_lann: "btzsandikaaa78", // Isi APIkey dari https://api.betabotz.eu.org
@@ -16,11 +16,11 @@ module.exports = {
   menuPublic: true, // Apakah menu public aktif?
 
   // Channel IDs
-  joinChannelId: "wajib di isi", // ID channel join
-  gempaChannelId: "wajib di isi", // ID channel gempa
-  growgardenChannelId: "wajib di isi", // ID channel growgarden
+  joinChannelId: "1386907338828419082", // ID channel join
+  gempaChannelId: "1384561242815070328", // ID channel gempa
+  growgardenChannelId: "1384561242815070328", // ID channel growgarden
   channelIds: {
-    rules: "wajib di isi", // ID channel rules
+    rules: "1386759038347247746", // ID channel rules
     pricelist: "wajib di isi", // ID channel pricelist
     ticket: "wajib di isi", // ID channel ticket
     queue: "wajib di isi", // ID channel queue
